aceaee2fde01e2a6d6e5393ac281110c0e652d0d
BeauArthurs <BeauArthurs@gmail.com>
2015-02-08 20:47:04 +0100

    Beau_005

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Vector3 direction;
+    [SerializeField]
+    private int speed;
+	void Start () 
+    {
+	}
+	
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                direction = new Vector3(0, 1, 0);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                direction = new Vector3(0, -1, 0);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                direction = new Vector3(-1, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                direction = new Vector3(1, 0, 0);
+            }
+            transform.position = transform.position + direction * Time.deltaTime * speed;
+        }
+	}
+}
